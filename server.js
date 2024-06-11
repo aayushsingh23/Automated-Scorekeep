fa6224a188e6f7d2a3755ec5c664a3a0b41d4a06
Aayush Singh <125759895+aayushsingh23@users.noreply.github.com>
2024-06-11 15:08:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 50000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`${process.env.PORT}`);
